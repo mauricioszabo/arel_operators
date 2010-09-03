c8ec68a91b064e48bf5c23e833b560e39cec2ccc
Maurício Szabo <mauricio.szabo@gmail.com>
2010-09-03 16:56:19 -0300

    Operatons now obey Joins.

--- lib/active_record/operators.rb
+++ lib/active_record/operators.rb
@@ -1,11 +1,11 @@
 module ActiveRecord
   module Operators
     def |(other)
-      build_arel_conditions build_predicate(Arel::Predicates::Or, self, other)
+      build_arel_predicate(Arel::Predicates::Or, self, other)
     end
 
     def &(other)
-      build_arel_conditions build_predicate(Arel::Predicates::And, self, other)
+      build_arel_predicate(Arel::Predicates::And, self, other)
     end
 
     def -(other)
@@ -13,29 +13,32 @@ module ActiveRecord
     end
 
     def -@
-      build_arel_conditions build_predicate(Arel::Predicates::Not, self)
+      build_arel_predicate(Arel::Predicates::Not, self)
     end
 
     def where(obj, *args)
-      if obj.respond_to?(:build_where)
-        relation = obj.select(:id)
-        self & build_arel_conditions(Arel::Predicates::In.new(relation.primary_key, relation.arel))
-      else
-        super
+      return super unless obj.respond_to?(:build_where)
+      relation = obj.select(:id)
+      self & clone.tap do |c|
+        cond = build_where(Arel::Predicates::In.new(relation.primary_key, relation.arel))
+        c.where_values = Array.wrap(cond)
       end
     end
 
     private
-    def build_arel_conditions(condition)
+    def build_arel_predicate(predicate, *args)
       clone.tap do |c|
+        condition = build_predicate(predicate, *args)
         cond = build_where(condition)
         c.where_values = Array.wrap(cond)
+        possible_joins = args.select { |x| x.object_id != object_id && x.respond_to?(:joins_values) }
+        c.joins_values += possible_joins.collect { |x| x.joins_values }
       end
     end
 
     def build_predicate(predicate, *relations)
       values = relations.collect do |relation|
-        value = relation.where_values
+        value = relation.is_a?(ActiveRecord::Relation) ? relation.where_values : relation
         wrap_predicate_value(value)
       end
       predicate.new(*values)
--- spec/active_record/joins_spec.rb
+++ spec/active_record/joins_spec.rb
@@ -5,11 +5,13 @@ describe ActiveRecord::Operators do
     p1 = Person.create! :name => 'Foo'
     p2 = Person.create! :name => 'Bar'
     p3 = Person.create! :name => 'Baz'
+    Address.create! :address => "Village", :person_id => p1.id
+    Address.create! :address => "Avenue", :person_id => p2.id
     Address.create! :address => "Road", :person_id => p3.id
 
     q1 = Person.where :name => 'Foo'
     q2 = Person.joins(:addresses).where('address = ?', 'Road')
-    result = q1 | q2
+    result = q2 | q1
     result.should include(p1)
     result.should include(p3)
     result.should_not include(p2)
